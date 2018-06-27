706ea5df8fe08c7bc509fb1648c9ff8a52fba965
Jacob Fullerton <fullertonj44@gmail.com>
2018-06-27 11:23:55 +0200

    delete forms

--- src/index.js
+++ src/index.js
@@ -6,22 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import { Actions, jsonformsReducer } from '@jsonforms/core';
-import { person } from '@jsonforms/examples';
-import { materialFields, materialRenderers } from '@jsonforms/material-renderers';
-
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
 
-const store = createStore(
-    combineReducers({ jsonforms: jsonformsReducer() }),  
-    {
-      jsonforms: {
-        renderers: materialRenderers,
-        fields: materialFields,
-      }
-    }
-  );
-
-store.dispatch(Actions.init(data, schema, uischema));
