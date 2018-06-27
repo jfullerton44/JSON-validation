c0712a4b00ae9da4fbaec79ff900fc75d29f682a
Jacob Fullerton <fullertonj44@gmail.com>
2018-06-27 11:39:19 +0200

    Remove Forms

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
