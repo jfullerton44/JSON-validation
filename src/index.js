ba912f5c1c9f915b932d9220e37640b4edb9fa82
Jacob Fullerton <fullertonj44@gmail.com>
2018-06-27 11:41:47 +0200

    Update index to compile

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import { Actions, jsonformsReducer } from '@jsonforms/core';
-import { person } from '@jsonforms/examples';
-import { materialFields, materialRenderers } from '@jsonforms/material-renderers';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
