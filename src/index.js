7ba690ee5be13f28de580e2ee6e2028a2821c851
eae4bf <35538100+eae4bf@users.noreply.github.com>
2018-06-28 09:33:45 +0200

    starting mongo

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ import { Provider } from 'react-redux';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb+srv://eliza:<N3z7v91MtqizC5f2>@cluster0-p4rbj.mongodb.net/test?retryWrites=true";
+MongoClient.connect(uri, function(err, client) {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   db.getSiblingDB("test").getCollection("foo").insert( { "msg" : "My First Document" } )
+   client.close();
+});
+
+
+
+
