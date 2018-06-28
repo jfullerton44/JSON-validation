28161028ac66b97f90d9c4873e8cec0f71ca90e1
Jacob Fullerton <fullertonj44@gmail.com>
2018-06-28 09:34:17 +0200

    update dependencies for mongodb

--- src/App.js
+++ src/App.js
@@ -18,4 +18,13 @@ class App extends Component {
   }
 }
 
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test";
+MongoClient.connect(uri, function(err, client) {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+
 export default App;
